b940ffe5a7a18cf99bff418bf6f7a4861b0f4c9a
clarlzx <clarlzx@gmail.com>
2021-06-15 10:30:06 +0800

    Set up firebase database

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './database/firebaseDB';
+
+// to add stuff to the collection in firebase
+// firebase.firestore().collection("attractions").add({
+//   name: "hello",
+// });
+
+// to retrieve stuff from firebase
+// firebase.firestore().collection("category").doc('themeWaterPark').get().then((doc) => {
+//   const name = doc.data().name;
+//   console.log(name);
+// })
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi</Text>
       <StatusBar style="auto" />
     </View>
   );
