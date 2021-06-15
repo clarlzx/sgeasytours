0600ea0630f3972f3811fbafc7ba89d6d528dfea
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-06-16 00:12:10 +0800

    added table

--- App.js
+++ App.js
@@ -11,6 +11,151 @@ import firebase from './database/firebaseDB';
 //   name: "hello",
 // });
 
+// firebase.firestore().collection("attractions").doc().set({
+//   pastWeek: [
+//     {
+//       day: "monday",
+//       time: [
+//         {id: "8am",crowd: 0},
+//         {id: "9am",crowd: 0},
+//         {id: "10am",crowd: 0},
+//         {id: "11am",crowd: 0},
+//         {id: "12pm",crowd: 0},
+//         {id: "1pm",crowd: 0},
+//         {id: "2pm",crowd: 0},
+//         {id: "3pm",crowd: 0},
+//         {id: "4pm",crowd: 0},
+//         {id: "5pm",crowd: 0},
+//         {id: "6pm",crowd: 0},
+//         {id: "7pm",crowd: 0},
+//         {id: "8pm",crowd: 0},
+//         {id: "9pm",crowd: 0},
+//         {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "tuesday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "wednesday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "thursday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "friday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "saturday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   },
+//   {
+//     day: "sunday",
+//     time: [
+//       {id: "8am",crowd: 0},
+//       {id: "9am",crowd: 0},
+//       {id: "10am",crowd: 0},
+//       {id: "11am",crowd: 0},
+//       {id: "12pm",crowd: 0},
+//       {id: "1pm",crowd: 0},
+//       {id: "2pm",crowd: 0},
+//       {id: "3pm",crowd: 0},
+//       {id: "4pm",crowd: 0},
+//       {id: "5pm",crowd: 0},
+//       {id: "6pm",crowd: 0},
+//       {id: "7pm",crowd: 0},
+//       {id: "8pm",crowd: 0},
+//       {id: "9pm",crowd: 0},
+//       {id: "10pm",crowd: 0}
+//     ]
+//   }
+// ]
+// }, {merge: true});
+
 // to retrieve stuff from firebase
 // firebase.firestore().collection("category").doc('themeWaterPark').get().then((doc) => {
 //   const name = doc.data().name;
