17764e033239477ac84ef6da99346a634c86e81c
clarlzx <clarlzx@gmail.com>
2021-06-17 03:33:01 +0800

    Add app name to background images

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,8 @@ import {
   View,
   ImageBackground,
   TouchableOpacity,
+  Image,
+  Dimensions,
 } from "react-native";
 import ScrollView from "../components/ScrollView";
 
@@ -12,9 +14,16 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("../assets/app-title.png")}
+        source={require("../assets/longer-bus-app-title.png")}
         style={styles.image}
       >
+        <View style={styles.titleImageView}>
+          <Image
+            style={styles.titleImage}
+            resizeMode="contain"
+            source={require("../assets/title.png")}
+          />
+        </View>
         <View style={styles.searchOuter}>
           <TouchableOpacity
             onPress={() => navigation.navigate("Search Screen")}
@@ -42,10 +51,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  titleImageView: {
+    height: "20%",
+    width: "100%",
+    alignItems: "flex-end",
+    paddingTop: 120,
+    paddingRight: 25,
+    justifyContent: "center",
+  },
+  titleImage: {
+    right: 0,
+    width: Dimensions.get("screen").width * 0.6,
+  },
   searchOuter: {
     backgroundColor: "#BA68C8",
-    width: "80%",
-    marginTop: 100,
+    width: "85%",
+    marginTop: 40,
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
@@ -53,8 +74,8 @@ const styles = StyleSheet.create({
   },
   searchInner: {
     backgroundColor: "#663a82",
-    borderRadius: 10,
-    width: "90%",
+    borderRadius: 5,
+    width: "100%",
     padding: 10,
   },
   searchText: {
