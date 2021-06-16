6d8fdef3cbf0d2e8ceacdb33aa03515f848063b8
clarlzx <clarlzx@gmail.com>
2021-06-17 00:25:44 +0800

    Edit user interface for RecommendationItems in FlatList

--- components/RecommendationItem.js
+++ components/RecommendationItem.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import { DataTable } from "react-native-paper";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 function bringLastWordToNextLine(str) {
   let name = "";
@@ -12,13 +19,92 @@ function bringLastWordToNextLine(str) {
 
   return name.substring(0, name.length - 1) + " " + nameArr.slice(-1);
 }
+
+function Name({ value }) {
+  return (
+    <View
+      style={{
+        width: Dimensions.get("screen").width * 0.42,
+        height: "100%",
+        justifyContent: "center",
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0,
+        borderRadius: 4,
+        borderColor: "white",
+        borderRightWidth: 1,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 15,
+          fontWeight: "bold",
+          paddingLeft: 15,
+          paddingRight: 10,
+        }}
+      >
+        {bringLastWordToNextLine(value)}
+      </Text>
+    </View>
+  );
+}
+
+function Distance({ value }) {
+  return (
+    <View
+      style={{
+        width: Dimensions.get("screen").width * 0.15,
+        padding: 5,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 16 }}>{value}</Text>
+      <Text style={{ fontSize: 16 }}>KM</Text>
+    </View>
+  );
+}
+
+function DrivingTime({ value }) {
+  return (
+    <View
+      style={{
+        width: Dimensions.get("screen").width * 0.15,
+        padding: 5,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <FontAwesome name="car" size={24} color="black" />
+      <Text>{value} min</Text>
+    </View>
+  );
+}
+
+function WalkingTime({ value }) {
+  return (
+    <View
+      style={{
+        width: Dimensions.get("screen").width * 0.15,
+        padding: 5,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <FontAwesome5 name="walking" size={24} color="black" />
+      <Text>{value} min</Text>
+    </View>
+  );
+}
+
 export default function ({ item, dataList, navigation }) {
   return (
     <View
       style={{
+        width: Dimensions.get("screen").width * 0.92,
         height: 70,
         flex: 1,
         justifyContent: "center",
+        alignSelf: "center",
         marginVertical: 8,
         marginHorizontal: 16,
         borderWidth: 1,
@@ -33,55 +119,34 @@ export default function ({ item, dataList, navigation }) {
           navigation.navigate("Details");
         }}
       >
-        <View
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
           style={{
-            width: "50%",
+            width: Dimensions.get("screen").width * 0.92,
             height: "100%",
-            justifyContent: "center",
-            borderBottomRightRadius: 0,
-            borderTopRightRadius: 0,
             borderRadius: 4,
-            borderColor: "white",
-            borderRightWidth: 1,
+            alignSelf: "center",
           }}
-        >
-          {/* <DataTable.Row>
-            <DataTable.Cell numeric>1</DataTable.Cell>
-            <DataTable.Cell numeric>2</DataTable.Cell>
-            <DataTable.Cell numeric>3</DataTable.Cell>
-            <DataTable.Cell numeric>4</DataTable.Cell>
-          </DataTable.Row> */}
-          <Text
-            style={{
-              left: 15,
-              fontSize: 15,
-              fontWeight: "bold",
-            }}
-          >
-            {bringLastWordToNextLine(item.name)}
-          </Text>
-          <Text
-            style={{
-              left: 15,
-              fontSize: 15,
-              fontWeight: "bold",
-            }}
-          >
-            {item.distance} KM
-          </Text>
-          {/* <FontAwesome name="car" size={24} color="black" /> */}
-        </View>
-        {/* <Text>
-          {item.name}
-          {"\n"}
-          {item.distance}km
-          {"\n"}
-          Driving: {dataList.drivingTime[item.name]} min Walking:{" "}
-          {dataList.walkingTime[item.name]} min
-        </Text> */}
+          data={[
+            { value: item.name, key: 1 },
+            { value: item.distance, key: 2 },
+            { value: dataList.drivingTime[item.name], key: 3 },
+            { value: dataList.walkingTime[item.name], key: 4 },
+          ]}
+          renderItem={({ item }) => {
+            if (item.key == 1) {
+              return <Name value={item.value} />;
+            } else if (item.key == 2) {
+              return <Distance value={item.value} />;
+            } else if (item.key == 3) {
+              return <DrivingTime value={item.value} />;
+            } else {
+              return <WalkingTime value={item.value} />;
+            }
+          }}
+        />
       </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
