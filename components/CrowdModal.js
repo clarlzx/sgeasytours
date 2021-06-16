33b76cf110d54ed37bf01de3f720fa0a927517dc
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2021-06-16 19:40:27 +0800

    crowdscreen should be done

---
+++ components/CrowdModal.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import Modal from "react-native-modal";
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+export default function CrowdModal({ day, time, data, percentage }) {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity style={
+        percentage < 50 ? styles.notcrowdedbutton : percentage < 75 ? styles.somecrowdbutton : styles.crowdedbutton
+       } onPress={toggleModal} />
+
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <Text style={styles.titletext}>Estimated Crowd</Text>
+            <Text style={styles.titletext}>({day}, {time})</Text>
+            <Text style={styles.datatext}>{data}</Text>
+          </View>
+
+          <TouchableOpacity style={styles.closeButton} onPress={toggleModal}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titletext: { fontWeight: "bold" },
+  datatext: { fontWeight:"bold", color: "grey", fontSize: 22 },
+  crowdedbutton: { flex: 1, backgroundColor: "red" },
+  somecrowdbutton: { flex: 1, backgroundColor: "pink" },
+  notcrowdedbutton: { flex: 1, backgroundColor: "green" },
+  container: {
+    //flex: 0.2,
+    height: 120,
+    width: 200,
+    backgroundColor: "white",
+    borderRadius: 10,
+    alignItems: "center",
+    alignSelf: "center"
+  },
+  textContainer: {
+    width: "100%",
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  closeButton: {
+    width: "100%",
+    height: "30%",
+    backgroundColor: "lightblue",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
